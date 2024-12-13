6fc5e2c93226553525fb03a968e3007f82f37b67
ronx-labs <ron.sam624@gmail.com>
2024-12-13 18:42:42 +0000

    feat: added OrderHistory model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 from database import Base
 
 class Proof(Base):
@@ -6,3 +6,10 @@ class Proof(Base):
 
     proof_key = Column(String, primary_key=True, index=True)
     data_hash = Column(String, index=True)
+
+class OrderHistory(Base):
+    __tablename__ = "order_history"
+
+    id=Column(Integer, autoincrement=True, primary_key=True, index=True)
+    user_id = Column(String, index=True)
+    minhash = Column(String, index=True)
