5da4400147fd630ee45aa6c27c19dbe44fc849a2
bradymillhouse11 <bradymillhouse11@gmail.com>
2024-12-13 19:08:11 -0500

    fix deps

--- app/api/deps.py
+++ app/api/deps.py
@@ -2,7 +2,8 @@
 from fastapi import FastAPI, HTTPException, Query, Security, Depends
 from fastapi.security.api_key import APIKeyHeader
 from sqlalchemy.orm import Session
-
+from app.db.session import get_db
+from app.core.config import settings
 
 API_KEY_NAME = "X-API-Key"
 api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=True)
