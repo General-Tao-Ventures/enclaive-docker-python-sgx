adf5bdfcd2cce4f98d0309158e8759b86a92eed9
ronx-labs <ron.sam624@gmail.com>
2025-01-07 02:32:28 +0000

    fix: update is_valid_amazon_link for s3 prefix

--- app/utils/misc.py
+++ app/utils/misc.py
@@ -18,37 +18,39 @@ async def download_and_hash(url):
 
 def is_valid_amazon_link(link):
     valid_domains = [
-        "s3.amazonaws.com.au",
-        "s3.amazonaws.com.be",
-        "s3.amazonaws.com.br",
-        "s3.amazonaws.ca",
-        "s3.amazonaws.cn",
-        "s3.amazonaws.eg",
-        "s3.amazonaws.fr",
-        "s3.amazonaws.de",
-        "s3.amazonaws.in",
-        "s3.amazonaws.it",
-        "s3.amazonaws.co.jp",
-        "s3.amazonaws.com.mx",
-        "s3.amazonaws.nl",
-        "s3.amazonaws.pl",
-        "s3.amazonaws.sa",
-        "s3.amazonaws.sg",
-        "s3.amazonaws.es",
-        "s3.amazonaws.se",
-        "s3.amazonaws.com.tr",
-        "s3.amazonaws.ae",
-        "s3.amazonaws.co.uk",
-        "s3.amazonaws.com",
-        "s3.amazonaws.co",
-        "s3.amazonaws.com.cn",
-        "s3.amazonaws.com.sg"
+        "amazonaws.com.au",
+        "amazonaws.com.be",
+        "amazonaws.com.br",
+        "amazonaws.ca",
+        "amazonaws.cn",
+        "amazonaws.eg",
+        "amazonaws.fr",
+        "amazonaws.de",
+        "amazonaws.in",
+        "amazonaws.it",
+        "amazonaws.co.jp",
+        "amazonaws.com.mx",
+        "amazonaws.nl",
+        "amazonaws.pl",
+        "amazonaws.sa",
+        "amazonaws.sg",
+        "amazonaws.es",
+        "amazonaws.se",
+        "amazonaws.com.tr",
+        "amazonaws.ae",
+        "amazonaws.co.uk",
+        "amazonaws.com",
+        "amazonaws.co",
+        "amazonaws.com.cn",
+        "amazonaws.com.sg"
     ]
     
     parsed_url = urlparse(link)
     if not parsed_url.scheme == "https":
         return False
     if not any(parsed_url.netloc.endswith(domain) for domain in valid_domains):
+        return False
+    if not ".s3." in parsed_url.netloc:
         return False
     if not (parsed_url.path.endswith("All%20Data%20Categories.zip")
             or parsed_url.path.endswith("All%20Data%20Categories.7.zip")
